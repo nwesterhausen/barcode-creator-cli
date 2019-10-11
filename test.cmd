88e17a2e5f3e87d00922aae8f0aca1dad662ee22
Nicholas Westerhausen <nwesterhausen@gmail.com>
2019-10-11 14:36:28 -0400

    create a simple test to run

---
+++ test.cmd
@@ -0,0 +1,4 @@
+node .\index.js -f QRCODE -o .\tmp\qr_test_svg.svg "Testing the svg qrcode"
+node .\index.js -f QRCODE -o .\tmp\qr_test_png.png "Testing the png qrcode"
+node .\index.js -f CODE128 -o .\tmp\128_test.svg "Testing the svg 128"
+node .\index.js -f CODE39 -o .\tmp\39_test.png "Testing the png 39"
